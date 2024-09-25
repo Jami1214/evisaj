676933e727f5658a44c425920a8d396a9ac63594
Jami1214 <jamilatusaeed3@gmail.com>
2024-09-25 16:06:30 +0100

    …

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,24 @@
 import "@/styles/globals.css";
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import 'keen-slider/keen-slider.min.css';
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-export default function App({ Component, pageProps }){}
+
+export default function App({ Component, pageProps }) {
   return (
     <>
-    <ToastContainer
-          theme="light"
-          position="top-right"
-          autoClose={4000}
-          closeOnClick
-          pauseOnHover={false}
-        /> 
-    <Component {...pageProps} />
-  </>);
+      <ToastContainer
+        theme="light"
+        position="top-right"
+        autoClose={4000}
+        closeOnClick
+        pauseOnHover={false}
+      />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
 
       
     
